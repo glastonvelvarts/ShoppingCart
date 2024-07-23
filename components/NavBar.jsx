1b52ade33123f1c314303614824c9faf92fc71a5
glastonvelvarts <glastonvelvarts@gmail.com>
2024-07-24 00:26:36 +0530

    commited

--- components/NavBar.jsx
+++ components/NavBar.jsx
@@ -4,7 +4,10 @@ import Search from "./Search";
 const NavBar = ({ searchTerm, setSearchTerm }) => {
   return (
     <header className="header">
-      <div className="logo">Shopping Site</div>
+      <div className="logo">
+        <span>Shopping Site</span>
+        <span className="cart-text">Shopping Cart</span>
+      </div>
       <nav className="nav">
         <a href="#home">Home</a>
         <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
